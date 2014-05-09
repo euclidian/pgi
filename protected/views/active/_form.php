fa193caedb5778d833ff01acb7a2ea14d4cbb020
13510103 <johan.ridho@gmail.com>
2014-05-09 12:54:42 +0700

    masukin gantchart

---
+++ protected/views/active/_form.php
@@ -0,0 +1,46 @@
+<?php
+/* @var $this ActiveController */
+/* @var $model Active */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'active-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'id_well'); ?>
+		<?php echo $form->textField($model,'id_well'); ?>
+		<?php echo $form->error($model,'id_well'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'active'); ?>
+		<?php echo $form->textField($model,'active'); ?>
+		<?php echo $form->error($model,'active'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'change_date'); ?>
+		<?php echo $form->textField($model,'change_date'); ?>
+		<?php echo $form->error($model,'change_date'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/views/active/_search.php
@@ -0,0 +1,40 @@
+<?php
+/* @var $this ActiveController */
+/* @var $model Active */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'id_well'); ?>
+		<?php echo $form->textField($model,'id_well'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'active'); ?>
+		<?php echo $form->textField($model,'active'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'change_date'); ?>
+		<?php echo $form->textField($model,'change_date'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
