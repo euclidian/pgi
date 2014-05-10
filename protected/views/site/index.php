b95221ce9e8959457bb85cefe6ec1d136aaa516e
13510103 <johan.ridho@gmail.com>
2014-05-10 19:24:00 +0700

    tambahin tema baru

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -5,21 +5,6 @@ $this->pageTitle=Yii::app()->name;
 ?>
 <link href="css/style.css" type="text/css" rel="stylesheet">
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
-
 <div class="contain">
 	<div class="gantt"></div>
 </div>
