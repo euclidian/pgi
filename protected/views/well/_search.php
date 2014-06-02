d85600dfe9fe39ef17c525c8d392ec7e2c0333ee
13510103 <johan.ridho@gmail.com>
2014-06-02 23:28:44 +0700

    search well hanya dg atribut name & api

--- protected/views/well/_search.php
+++ protected/views/well/_search.php
@@ -11,10 +11,7 @@
 	'method'=>'get',
 )); ?>
 
-	<div class="row">
-		<?php echo $form->label($model,'id'); ?>
-		<?php echo $form->textField($model,'id'); ?>
-	</div>
+	
 
 	<div class="row">
 		<?php echo $form->label($model,'name'); ?>
@@ -26,30 +23,7 @@
 		<?php echo $form->textField($model,'api',array('size'=>60,'maxlength'=>255)); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->label($model,'active'); ?>
-		<?php echo $form->textField($model,'active'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'production'); ?>
-		<?php echo $form->textField($model,'production',array('size'=>60,'maxlength'=>65)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'note'); ?>
-		<?php echo $form->textField($model,'note',array('size'=>60,'maxlength'=>255)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'id_lease'); ?>
-		<?php echo $form->textField($model,'id_lease',array('size'=>60,'maxlength'=>255)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'last_update'); ?>
-		<?php echo $form->textField($model,'last_update'); ?>
-	</div>
+	
 
 	<div class="row buttons">
 		<?php echo CHtml::submitButton('Search'); ?>
